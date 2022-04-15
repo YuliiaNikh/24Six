316b00b987de7d5fc332df52d7c5cb96245d188c
Yuliia Nikhaienko <yuliia_nikhaienko@epam.com>
2022-04-15 15:55:04 +0300

    initial commit

---
+++ assets/scripts/js.js
@@ -0,0 +1,25 @@
+//Dashed line between two cards
+const card = document.querySelectorAll('.step-card');
+const cardImage = document.querySelectorAll('.step-card .card-header');
+let dashedLineContainers = document.querySelectorAll('.dashed-line');
+
+function drawDashedLine(){
+    let distanceBetweenCards = (card[1].getBoundingClientRect().left - card[0].getBoundingClientRect().right + cardImage[0].offsetLeft);
+
+    for (const element of dashedLineContainers) {
+        element.style.width = distanceBetweenCards+'px';
+    }
+    for (let i = 0; i < 2; i++){
+        dashedLineContainers[i].style.left = (card[i].offsetLeft + cardImage[i].offsetLeft + cardImage[i].offsetWidth + 44)+'px';
+    }
+}
+
+drawDashedLine();
+
+window.addEventListener('resize', function (event) {
+    drawDashedLine()
+}, true);
+
+//Footer current year
+document.getElementById("year").innerHTML = new Date().getFullYear();
+
---
+++ assets/scripts/swipper.js
@@ -0,0 +1,93 @@
+document.addEventListener("DOMContentLoaded", function () {
+    let featuresSlideNumber = document.getElementById('slide-item__number');
+
+    function visualisedSlideNumber(slideIndex){
+        if (slideIndex < 9){
+            featuresSlideNumber.innerText =`0${slideIndex + 1}`;
+        } else {
+            featuresSlideNumber.innerText = slideIndex + 1;
+        }
+    }
+
+    let swiper = new Swiper(".testimonials-carousel", {
+        slidesPerView: "auto",
+        spaceBetween: 32,
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+        },
+    });
+
+    let swiper2 = new Swiper(".features-slider-text", {
+        autoHeight: true,
+        slidesPerView: 1,
+        spaceBetween: 20,
+        mousewheel: true,
+        breakpoints: {
+            768: {
+                autoHeight: false,
+                loop: true,
+                direction: 'vertical',
+                slidesPerView: 3,
+                pagination: {
+                    el: ".swiper-pagination",
+                    type: 'progressbar',
+                },
+            }
+        },
+        on:{
+            init: function (){
+                visualisedSlideNumber(this.realIndex);
+                // featuresSlideNumber.innerText =`0${this.realIndex + 1}`;
+            },
+            slideChange: function () {
+                visualisedSlideNumber(this.realIndex)
+                // featuresSlideNumber.innerText =`0${this.realIndex + 1}`;
+            }
+        }
+    });
+
+    let swiper3 = new Swiper(".features-slider-img", {
+        spaceBetween: 10,
+        mousewheel: true,
+        breakpoints: {
+            768: {
+                loop: true,
+                direction: 'vertical',
+            }
+        },
+    })
+
+    swiper3.params.control = swiper2;
+    swiper2.params.control = swiper3;
+
+    let isSliding = false;
+
+    function handleSlide(swiperInstance, swiperDirection) {
+        if (isSliding) return;
+        isSliding = true;
+        if (swiperDirection === 0) {
+            swiperInstance.slideNext();
+        } else {
+            swiperInstance.slidePrev();
+        }
+        isSliding = false;
+    }
+
+    swiper2.on('slideNextTransitionStart', () => {
+        handleSlide(swiper3, 0);
+    })
+
+    swiper2.on('slidePrevTransitionStart', () => {
+        handleSlide(swiper3, 1);
+    })
+
+    swiper3.on('slideNextTransitionStart', () => {
+        handleSlide(swiper2, 0);
+    })
+
+    swiper3.on('slidePrevTransitionStart', () => {
+        handleSlide(swiper2, 1);
+    })
+});
+
