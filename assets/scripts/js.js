a95e36bac460a2304ce773f18d6057d34eea0a87
Yuliia Nikhaienko <yuliia_nikhaienko@epam.com>
2022-05-20 13:13:37 +0300

    hide collapsible navbar on click

--- assets/scripts/js.js
+++ assets/scripts/js.js
@@ -1,3 +1,11 @@
+const navLinks = document.querySelectorAll('.nav-item')
+const menuToggle = document.getElementById('navbarSupportedContent')
+const bsCollapse = new bootstrap.Collapse(menuToggle, {toggle:false})
+navLinks.forEach((l) => {
+    l.addEventListener('click', () => { bsCollapse.toggle() })
+})
+
+
 // Change navbar background color and hide back link on scroll
 const navbar = document.getElementById('navbar');
 const backLink = document.getElementById('back-link');
