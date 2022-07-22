d4e4902607b2d4b20a31726789c415f78686a770
Yuliia Nikhaienko <yuliia_nikhaienko@epam.com>
2022-07-22 19:59:03 +0300

    oss features slider

--- assets/scripts/swipper.js
+++ assets/scripts/swipper.js
@@ -18,93 +18,93 @@ document.addEventListener("DOMContentLoaded", function () {
         },
     });
 
-    let swiper2 = new Swiper(".features-slider-text", {
-        autoHeight: true,
-        slidesPerView: 1,
-        spaceBetween: 20,
-        updateOnWindowResize: true,
-        mousewheel: {
-            forceToAxis: true,
-            sensitivity: 1,
-            releaseOnEdges: true,
-        },
-        pagination: {
-            el: ".swiper-pagination",
-            type: 'progressbar',
-        },
-        on: {
-            init: function () {
-                visualisedSlideNumber(this.realIndex);
-            },
-            slideChange: function () {
-                visualisedSlideNumber(this.realIndex);
-            },
-            reachEnd: function() {
-                this.snapGrid = [...this.slidesGrid];
-            },
-        },
-        breakpoints: {
-            768: {
-                direction: 'vertical',
-                autoHeight: false,
-                slidesPerView: 'auto',
-                loopFillGroupWithBlank: true,
-            }
-        },
-
-    });
-
-    let swiper3 = new Swiper(".features-slider-img", {
-        spaceBetween: 10,
-        mousewheel: {
-            forceToAxis: true,
-            sensitivity: 1,
-            releaseOnEdges: true,
-        },
-        breakpoints: {
-            768: {
-                direction: 'vertical',
-                loopFillGroupWithBlank: true,
-                on:{
-                    reachEnd: function() {
-                        this.snapGrid = [...this.slidesGrid];
-                    },
-                }
-            }
-        },
-    })
-
-    swiper3.params.control = swiper2;
-    swiper2.params.control = swiper3;
-
-    let isSliding = false;
-
-    function handleSlide(swiperInstance, swiperDirection) {
-        if (isSliding) return;
-        isSliding = true;
-        if (swiperDirection === 0) {
-            swiperInstance.slideNext();
-        } else {
-            swiperInstance.slidePrev();
-        }
-        isSliding = false;
-    }
-
-    swiper2.on('slideNextTransitionStart', () => {
-        handleSlide(swiper3, 0);
-    })
-
-    swiper2.on('slidePrevTransitionStart', () => {
-        handleSlide(swiper3, 1);
-    })
-
-    swiper3.on('slideNextTransitionStart', () => {
-        handleSlide(swiper2, 0);
-    })
-
-    swiper3.on('slidePrevTransitionStart', () => {
-        handleSlide(swiper2, 1);
-    })
+    // let swiper2 = new Swiper(".features-slider-text", {
+    //     autoHeight: true,
+    //     slidesPerView: 1,
+    //     spaceBetween: 20,
+    //     updateOnWindowResize: true,
+    //     mousewheel: {
+    //         forceToAxis: true,
+    //         sensitivity: 1,
+    //         releaseOnEdges: true,
+    //     },
+    //     pagination: {
+    //         el: ".swiper-pagination",
+    //         type: 'progressbar',
+    //     },
+    //     on: {
+    //         init: function () {
+    //             visualisedSlideNumber(this.realIndex);
+    //         },
+    //         slideChange: function () {
+    //             visualisedSlideNumber(this.realIndex);
+    //         },
+    //         reachEnd: function() {
+    //             this.snapGrid = [...this.slidesGrid];
+    //         },
+    //     },
+    //     breakpoints: {
+    //         768: {
+    //             direction: 'vertical',
+    //             autoHeight: false,
+    //             slidesPerView: 'auto',
+    //             loopFillGroupWithBlank: true,
+    //         }
+    //     },
+    //
+    // });
+    //
+    // let swiper3 = new Swiper(".features-slider-img", {
+    //     spaceBetween: 10,
+    //     mousewheel: {
+    //         forceToAxis: true,
+    //         sensitivity: 1,
+    //         releaseOnEdges: true,
+    //     },
+    //     breakpoints: {
+    //         768: {
+    //             direction: 'vertical',
+    //             loopFillGroupWithBlank: true,
+    //             on:{
+    //                 reachEnd: function() {
+    //                     this.snapGrid = [...this.slidesGrid];
+    //                 },
+    //             }
+    //         }
+    //     },
+    // })
+    //
+    // swiper3.params.control = swiper2;
+    // swiper2.params.control = swiper3;
+    //
+    // let isSliding = false;
+    //
+    // function handleSlide(swiperInstance, swiperDirection) {
+    //     if (isSliding) return;
+    //     isSliding = true;
+    //     if (swiperDirection === 0) {
+    //         swiperInstance.slideNext();
+    //     } else {
+    //         swiperInstance.slidePrev();
+    //     }
+    //     isSliding = false;
+    // }
+    //
+    // swiper2.on('slideNextTransitionStart', () => {
+    //     handleSlide(swiper3, 0);
+    // })
+    //
+    // swiper2.on('slidePrevTransitionStart', () => {
+    //     handleSlide(swiper3, 1);
+    // })
+    //
+    // swiper3.on('slideNextTransitionStart', () => {
+    //     handleSlide(swiper2, 0);
+    // })
+    //
+    // swiper3.on('slidePrevTransitionStart', () => {
+    //     handleSlide(swiper2, 1);
+    // })
 });
 
 
